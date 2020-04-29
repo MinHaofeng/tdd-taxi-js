6bf54526768c9540b76dc5239654af02dd43e832
MinHaofeng <min9913@126.com>
2020-04-29 16:23:20 +0800

    测试优化

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,4 +1,4 @@
 import main from '../index'
 test('should ', () => {
-  expect(main('testData.txt')).toBe('收费6元\n收费7元\n收费13元\n收费7元\n');
+  expect(main()).toBe('收费6元\n收费7元\n收费13元\n收费7元\n');
 });
--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function calculatePrice(mile, time) {
 function getNumberInStr(str) {
   return str.replace(/[^0-9]/ig, "")
 }
-export default function main(testDataFile) {
+export default function main(testDataFile = 'testData.txt') {
   /* TODO
     1. testDataFile为fixtures文件夹下的测试数据文件名，例如传入的值为"testData.txt"，注意并不包含文件路径。
     2. 你写的程序将把testDataFile作为参数加载此文件并读取文件内的测试数据，并对每条测试数据计算结果。
